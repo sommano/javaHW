e4aa483c91a2aacb510aa4890a12b48ad8c33b7c
sommano <sommano@gmail.com>
2018-04-12 17:41:45 -0400

    first commit

---
+++ MyFirstJavaProgram/MyFirstJavaProgram.java
@@ -0,0 +1,11 @@
+public class MyFirstJavaProgram {
+    /*This is my first java program
+    this will print Hello World as the output
+    This is an example of multi-line comments*/
+
+    public static void main(String []args) {
+        //This is an example of single line comment
+        /*this is also an example of single line commment */
+        System.out.println("Hello World");
+    }
+}
