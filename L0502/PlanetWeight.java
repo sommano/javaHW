20eca16d80dac17ecf57f868f41c03d5ca2e10b9
Sommano Phivilay <34316590+sommano@users.noreply.github.com>
2018-04-29 00:01:26 -0400

    cha6

---
+++ L0502/PlanetWeight.java
@@ -0,0 +1,21 @@
+package com.java24hours;
+
+class PlanetWeight {
+    public static void main(String[] arguments) {
+        System.out.print("Your weight on earth is ");
+        double weight = 178;
+        System.out.println(weight);
+
+        System.print("Your weight on Mervury is ");
+        double mercury = weight * 0.378;
+        System.out.println(mercury);
+
+        System.out.print("Your weight on the Moon is ");
+        double moon = weight * 0.166;
+        System.out.println(moon);
+
+        System.out.print("Your weight on Jupitor is ");
+        double jupitor = weight * 2.364;
+        System.out.println(jupitor);
+    }
+}
