c8af0ec4c0566f934745b6227c1570aabd8cfd8f
Sommano Phivilay <34316590+sommano@users.noreply.github.com>
2018-04-30 23:51:09 -0400

    cha9

---
+++ L0902/NameSorter.java
@@ -0,0 +1,10 @@
+package com.java24hours;
+
+import java.util.*;
+
+class NameSorter {
+    public static void main(String[] arguments) {
+        String names[] = { "Glimmer", "Marvel", "Rue", "Clove",
+            "Thresh", "Foxface", }
+    }
+}
