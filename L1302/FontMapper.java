ead74c535055db311141535e1668d6e34427573b
Sommano Phivilay <34316590+sommano@users.noreply.github.com>
2018-05-05 17:51:59 -0400

    cha13

---
+++ L1302/FontMapper.java
@@ -0,0 +1,11 @@
+package com.java24hours;
+
+import java.awt.*;
+import java.util.*;
+
+public class FontMapper {
+    public FontMapper() {
+        Font courier = new Font("Courier New", Font.PLAIN, 6);
+        
+    }
+}
---
+++ L1401/Calculator.java
@@ -0,0 +1,11 @@
+package com.java24hours;
+
+public class Calculator {
+    public static void main(String[] arguments) {
+        float sum = 0;
+        for (String argument : arguments) {
+            sum = sum + Float.parseFloat(argument);
+        }
+        System.out.println("Those numbers add up to " + sum);
+    }
+}
